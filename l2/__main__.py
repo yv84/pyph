275224789f771a2dfdfd876edcdf3a59f33e09d2
Vladimir Yudintsev <yv87@ya.ru>
2013-03-20 13:14:06 +0600

    Update __main__.py

--- l2/__main__.py
+++ l2/__main__.py
@@ -4,13 +4,13 @@ import os.path
 import sys
 
 def getExecPath():
-    try:
-        sFile = os.path.abspath(sys.modules['__main__'].__file__)
-    except:
-        sFile = sys.executable
-    return os.path.dirname(sFile)
+        try:
+            sFile = os.path.abspath(sys.modules['__main__'].__file__)
+        except:
+            sFile = sys.executable
+        return os.path.dirname(sFile)
 
 if __name__ == "__main__":
-  path = getExecPath()
-  print(path)
-  __init__.run(path)
+    path = getExecPath()
+    print(path)
+    __init__.run(path)
