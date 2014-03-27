3bceb6f3840b699b275580be44e7abeccbd96709
Vladimir Yudintsev <yv87@ya.ru>
2014-03-28 01:13:23 +0600

    test c/s pinpong

--- testing/test1.py
+++ testing/test1.py
@@ -8,6 +8,7 @@ import sys
 import os
 import time
 import io
+import re
 from subprocess import Popen
 from multiprocessing import Process, Manager, JoinableQueue
 
@@ -23,10 +24,6 @@ class TestCase(unittest.TestCase):
     def testSimple(self):
         self.assertTrue(True)
 
-    class DummyFile(object):
-        def open(self, x): pass
-        def write(self, x): pass
-        def flush(self): pass
 
     def testEchoServer(self):
         self.port1 = '9999'
@@ -86,6 +83,40 @@ class TestCase(unittest.TestCase):
 
 
 
+    def testMessageClass(self):
+        from testing.msg_log import Message
+        from testing.game_log import log
+        _log = []
+        _side_log = {'client': [], 'server': []}
+        i = 0
+        while log:
+            if log and log[0].get('C'):
+                _side_log['client'].append(i)
+                _log.append(log.pop(0)['C'])
+            elif log and log[0].get('S'):
+                _side_log['server'].append(i)
+                _log.append(log.pop(0)['S'])
+            else:
+                raise Exception("S/C key wrong")
+            i += 1
+        # with open(os.path.dirname(__file__) + '/game_log_15122012', 'r') as f:
+        message_client = Message('client', log=_log, side_log=_side_log)
+        message_server = Message('server', log=_log, side_log=_side_log)
+        self.assertTrue(b''.join(message_client(b'')) == _log[0])
+        self.assertTrue(b''.join(message_server(_log[0])) == _log[1])
+        self.assertTrue(b''.join(message_client(_log[1])) == _log[2])
+        self.assertTrue(b''.join(message_server(_log[2])) == b''.join(_log[3:4+1]))
+        self.assertTrue(b''.join(message_client(b''.join(_log[3:4+1]))) == b''.join(_log[5:6+1]))
+
+
+
+def get_exec_path():
+        try:
+            sFile = os.path.abspath(sys.modules['__main__'].__file__)
+        except:
+            sFile = sys.executable
+        return os.path.dirname(sFile)
 
 if __name__ == '__main__':
+    print(get_exec_path())
     unittest.main()
