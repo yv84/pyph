9c963d08489e582bd7ff1abef2d8e975cf62645a
Vladimir Yudintsev <yv87@ya.ru>
2014-04-26 20:04:58 +0600

    web socket routing

--- src/proxy/ws_server.py
+++ src/proxy/ws_server.py
@@ -5,13 +5,34 @@ import websockets
 
 @asyncio.coroutine
 def hello(websocket, path):
-    print(websocket)
     name = yield from websocket.recv()
     print("s:< {}".format(name))
     greeting = "Hello {}!".format(name)
     print("s:> {}".format(greeting))
     yield from websocket.send(greeting)
 
+# @app.route('/')
+@asyncio.coroutine
+def  index(websocket, path):
+    response = '<h1>Index page</h1><a href="/async">async</a>, <a href="/demo">async-demo</a>, <a href="/flask">flask</a>'
+#       '<p>' + repr(loop) + '</p>' + '<p>' + repr(app) + '</p>' + \
+#       '<p>' + repr(manager.data) + '</p>' +\
+#       repr(b''.join([b'<p><ol><li>',
+#           (b'</li><li>'.join(manager.packets)),
+#           b'</li></ol></p>']))
+    print("s:> {}".format(response))
+    yield from websocket.send(response)
+
+@asyncio.coroutine
+def site(websocket, path):
+    print(path, end=' , ')
+    print(websocket)
+    if path == ('/'):
+        yield from index(websocket, path)
+    elif path.startswith('/hello'):
+        yield from hello(websocket, path)
+
+
 def websocket_serve(loop):
-    start_server = yield from websockets.serve(hello, 'localhost', 8765)
+    start_server = yield from websockets.serve(site, 'localhost', 8765)
 
--- testing/ws_client.py
+++ testing/ws_client.py
@@ -12,6 +12,14 @@ def hello():
     print("c:> {}".format(name))
     greeting = yield from websocket.recv()
     print("c:< {}".format(greeting))
+    
+    websocket = yield from websockets.connect('ws://localhost:8765/hello')
+    #name = input("What's your name? ")
+    name = "test"
+    yield from websocket.send(name)
+    print("c:> {}".format(name))
+    greeting = yield from websocket.recv()
+    print("c:< {}".format(greeting))
 
 def main():
     asyncio.get_event_loop().run_until_complete(hello())
