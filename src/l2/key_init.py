f249839b4ab0f29188a531444f50194a7353813d
Vladimir Yudintsev <yv87@ya.ru>
2014-07-05 14:58:18 +0600

    refactoring l2/len_packet.py

--- src/l2/key_init.py
+++ src/l2/key_init.py
@@ -1,7 +1,7 @@
 import itertools
 import numpy
 
-from .len_packet import LenL2PacketRcv, LenL2PacketSend
+from .len_packet import LenPackets
 from .xor import Xor
 from .gs_l2_packet import PacketError
 
@@ -23,15 +23,14 @@ class KeyInit():
         if to_s_d.startswith(b'\x19\x00.'):
             for stack, obj in zip([self.packet.client.command_stack, self.packet.server.command_stack],
                      [self.packet.client, self.packet.server]):
-                stack.append(lambda data: obj.pck_rcv.segmentation_packets(data))
+                stack.append(lambda data: obj.pck_len.pck_in(data))
                 stack.append(lambda gen: obj.xor_in.xor(gen))
                 stack.append(lambda gen, manager=self.packet.manager, name=obj.name,
                    peername=self.packet.peername : self.packet.manager.set_manager_data(name, gen, peername))
                 stack.append(lambda gen, name=obj.name, gameapi=self.gameapi, peername=self.packet.peername: \
                     self.packet_print_dtype(name, gameapi, gen, peername))
                 stack.append(lambda gen: obj.xor_out.xor(gen))
-                stack.append(lambda gen: obj.pck_send.add_packets(gen))
-                stack.append(lambda gen: obj.pck_send.pop_packet()) # -> bytes(data)
+                stack.append(lambda gen: obj.pck_len.pck_out(gen))
             self.packet.server.command_stack.append(lambda data: key_packet_initialization_remover(data))
         return to_s_data
 
@@ -63,7 +62,6 @@ class Connect():
         self.name = name
         self._data = b''
         self.command_stack = [] # func(gen: types.GeneratorType) -> types.GeneratorType
-        self.pck_rcv = LenL2PacketRcv()
-        self.pck_send = LenL2PacketSend()
+        self.pck_len = LenPackets()
         self.xor_in = Xor('decode')
         self.xor_out = Xor('code')
