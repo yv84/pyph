f34752ae5b10b1fb6f791e5ae88c38ec2603ade4
Vladimir Yudintsev <yv87@ya.ru>
2014-10-25 15:46:25 +0600

    test l2.packet_pipe

--- src/l2/test/test_l2.py
+++ src/l2/test/test_l2.py
@@ -1,6 +1,6 @@
 # /usr/bin/python3
 
-# python3 -m unittest -v testing.test1
+# python3 -m unittest l2.test.test_l2
 import unittest
 from unittest.mock import MagicMock
 from unittest.mock import patch
